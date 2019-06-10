9f284bc89cf45f28881cc278297e853aa765e79d
Jinghua Zhu <jhzhu@outlook.com>
2019-06-10 19:22:52 +0800

    Add global types

---
+++ pkg/types/types.go
@@ -0,0 +1,8 @@
+package types
+
+const (
+	// DefaultCRDNamespace is the default namespace where we create CRD instances.
+	DefaultCRDNamespace string = "crd-ns"
+	// DefaultKubeConfigPath is the default local path of kubeconfig file.
+	DefaultKubeConfigPath string = "~/.kube/config"
+)
