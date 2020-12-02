fbdb9e7cb4c886b574160d679858323c5e2cc010
Jinghua Zhu <jhzhu@outlook.com>
2020-12-02 19:03:45 +0800

    Add duplicate jinghzhu:v1alpha for demo

---
+++ pkg/crd/jinghzhu/v1alpha/register.go
@@ -0,0 +1,46 @@
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+)
+
+const (
+	Kind string = "Jinghzhu"
+	// GroupVersion is the version.
+	GroupVersion string = "v1"
+	// Plural is the Plural for Jinghzhu.
+	Plural string = "jinghzhus"
+	// Singular is the singular for Jinghzhu.
+	Singular string = "jinghzhu"
+	// CRDName is the CRD name for Jinghzhu.
+	CRDName string = Plural + "." + GroupName
+)
+
+var (
+	// SchemeGroupVersion is the group version used to register these objects.
+	SchemeGroupVersion = schema.GroupVersion{
+		Group:   GroupName,
+		Version: GroupVersion,
+	}
+	SchemeBuilder = runtime.NewSchemeBuilder(addKnownTypes)
+	AddToScheme   = SchemeBuilder.AddToScheme
+)
+
+// Resource takes an unqualified resource and returns a Group qualified GroupResource
+func Resource(resource string) schema.GroupResource {
+	return SchemeGroupVersion.WithResource(resource).GroupResource()
+}
+
+// addKnownTypes adds the set of types defined in this package to the supplied scheme.
+func addKnownTypes(scheme *runtime.Scheme) error {
+	scheme.AddKnownTypes(SchemeGroupVersion,
+		&Jinghzhu{},
+		&JinghzhuList{},
+	)
+	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
+
+	return nil
+}
