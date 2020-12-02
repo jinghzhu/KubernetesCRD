1e7144a68e1c6deb4e8647a3152d98dece8c268f
Jinghua Zhu <jhzhu@outlook.com>
2020-12-02 19:18:03 +0800

    Rename default CRD namespace as crd

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -2,7 +2,7 @@ package types
 
 const (
 	// DefaultCRDNamespace is the default namespace where we create CRD instances.
-	DefaultCRDNamespace string = "crd-ns"
+	DefaultCRDNamespace string = "crd"
 	// DefaultKubeConfigPath is the default local path of kubeconfig file.
 	DefaultKubeConfigPath string = "~/.kube/config"
 )
